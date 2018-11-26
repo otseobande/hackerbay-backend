139fa277bd685af7ba641393a3205d793b83d95f
Obande Otse <otseobande@gmail.com>
2018-11-26 22:49:04 +0100

    feat(json-patch): implement json patch (#8)
    - install necessary packages
    - write controllers and routes
    - write test for json patch endpoint
    
    [Delivers #162196145]

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import authRouter from './authRouter';
 import thumbnailRouter from './thumbnailRoutes';
+import jsonPatchRouter from './jsonPatchRouter';
 
 const apiRouter = Router();
 
-apiRouter.use('/api/v1', authRouter, thumbnailRouter);
+apiRouter.use('/api/v1', authRouter, thumbnailRouter, jsonPatchRouter);
 
 export default apiRouter;
